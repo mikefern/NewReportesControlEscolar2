bdfa481b6d8f04c7c5f5b501da7f870c65cfba1b
Fausto <miguel_rog@hotmail.com>
2022-05-30 15:49:32 -0500

    cambio

--- NewReportesControlEscolar2/Form1.Designer.cs
+++ NewReportesControlEscolar2/Form1.Designer.cs
@@ -45,7 +45,7 @@ namespace NewReportesControlEscolar2
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(363, 214);
+            this.button2.Location = new System.Drawing.Point(198, 224);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
