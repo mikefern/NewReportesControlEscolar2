16fa60934681f55f6598e6ebf3309af2ee31539a
Miguel <miguel_rog@hotmail.com>
2022-06-06 19:54:35 -0500

    new mrfr

--- NewReportesControlEscolar/Principal.Designer.cs
+++ NewReportesControlEscolar/Principal.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace NewReportesControlEscolar
 {
-    partial class Form1
+    partial class Principal
     {
         /// <summary>
         /// Required designer variable.
--- NewReportesControlEscolar/Principal.cs
+++ NewReportesControlEscolar/Principal.cs
@@ -11,9 +11,9 @@ using ProyectoLoboSostenido;
 
 namespace NewReportesControlEscolar
 {
-    public partial class Form1 : Form
+    public partial class Principal : Form
     {
-        public Form1()
+        public Principal()
         {
             InitializeComponent();
         }
